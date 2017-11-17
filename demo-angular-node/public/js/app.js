25fa2da02aae17d5824ee1073a551fd2c2d48be1
maikhanh123 <maikhanh123@gmail.com>
2017-11-17 13:27:05 +0700

    change cover and nabar

--- demo-angular-node/public/js/app.js
+++ demo-angular-node/public/js/app.js
@@ -1,1 +1,9 @@
-angular.module('myApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NewsCtrl', 'DetailCtrl', 'StoreCtrl']);
+var app = angular.module('myApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NewsCtrl', 'DetailCtrl', 'StoreCtrl']);
+
+
+app.directive("navBar",function () {
+    return{
+        restric: "AEC",
+        templateUrl : "views/navBar.html"
+    }
+});
